eb740544f6a446ae0778b1e2c7dfa141486b7964
jagotaa <arjun.jagota@rmp.uhn.ca>
2024-04-06 11:34:08 +0100

    adding dist_to_recovery in gen_policies

--- gen_policies.py
+++ gen_policies.py
@@ -10,40 +10,6 @@ from shaping_features import smallest_distance_to_recovery
 import pickle
 import numpy as np
 
-# def experiment_actions(nb_episodes, env, action_probs):
-#     """
-#     Run the experiment for a specified number of episodes.
-
-#     Parameters:
-#     - nb_episodes: Number of episodes
-#     - env: Experiment environment
-#     - action_probs: Action probabilities
-
-#     Returns:
-#     - policies: List of policies (pi_b or pi_e)
-#     """
-#     policies = []
-#     for i in range(nb_episodes):
-#         trajectory = []
-#         s = env.reset()
-#         env.render()
-#         term = False
-#         timestep = 0
-#         while not term:
-#             state_last = s
-#             # action = get_e_greedy_action(s, epsilon, qnet, explore = True)
-#             action = choose_action(tuple(s), action_probs)
-#             s, r, term, _ = env.step(action)
-#             # trajectory.append([state_last, action, r, s])
-#             psi = smallest_distance_to_deadend(state_last, env)
-#             trajectory.append(np.array([state_last, action, r, s, timestep, psi]))
-#             timestep +=1
-
-#         policies.append(trajectory)
-#     with open('policies.pkl', 'wb') as f:
-#         pickle.dump(policies, f)
-#     return policies
-
 
 def experiment_actions(nb_episodes, env, action_probs):
     """
@@ -64,7 +30,9 @@ def experiment_actions(nb_episodes, env, action_probs):
         ('reward', np.float64),
         ('state_next', np.float64, (2,)),
         ('timestep', np.int64),
-        ('psi', np.float64)
+        # ('dead_end_dist', np.float64),
+        # ('recovery_dist', np.float64),
+        ('psi', np.float64, (2,))
     ]
 
     policies = []
@@ -79,7 +47,9 @@ def experiment_actions(nb_episodes, env, action_probs):
             action = choose_action(tuple(s), action_probs)
             s, r, term, _ = env.step(action)
             
-            psi = smallest_distance_to_deadend(state_last, env)
+            dead_end_dist = smallest_distance_to_deadend(state_last, env)
+            recovery_dist = smallest_distance_to_recovery(state_last, env)
+            psi = [dead_end_dist, recovery_dist]            
             data_point = np.array([(state_last, action, r, s, timestep, psi)], dtype=dtype)
             trajectory = np.append(trajectory, data_point)
             timestep += 1
